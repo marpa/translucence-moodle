f1381948d832e6e3cb7007d288cd2fa948a38a9c
Alex Chapin <achapin@gmail.com>
2011-08-10 15:51:17 -0400

    sample markup for dropdown menus

---
+++ menu-sample.php
@@ -0,0 +1,123 @@
+    <div id="dropdown" class="yuimenubar yuimenubarnav">
+      <div class="bd">
+      
+        <ul class="first-of-type">
+        
+        	<li class="yuimenubaritem first-of-type"><a class="yuimenubaritemlabel" href="<?php echo $CFG->wwwroot; ?>">Home</a></li>
+          <li class="yuimenubaritem"><a class="yuimenubaritemlabel" href="#">First Menu</a>
+          
+            <div class="yuimenu">
+              <div class="bd">
+                <ul>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fourth Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fifth Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Sixth Menu Item</a>
+                  
+                    <div class="yuimenu">
+                      <div class="bd">
+                        <ul class="first-of-type">
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Sub Item</a></li>
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Sub Item</a></li>
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Sub Item</a>
+                            
+                            <div class="yuimenu">
+                              <div class="bd">
+                                <ul class="first-of-type">
+                                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Sub Sub Item</a></li>
+                                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Sub Sub Item</a></li>
+                                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Sub Sub Item</a>
+                                    
+                                    <div class="yuimenu">
+                                      <div class="bd">
+                                        <ul class="first-of-type">
+                                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Sub Sub Sub Item</a></li>
+                                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Sub Sub Sub Item</a></li>
+                                         <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Sub Sub Sub Item</a></li>
+                                        </ul>
+                                      </div>
+                                    </div>
+                                    
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                            
+                          </li>
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fourth Sub Item</a></li>
+                        </ul>            
+                      </div>
+                    </div>
+                                        
+                  </li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Seventh Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Eighth Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Ninth Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Tenth Menu Item</a></li>
+                </ul>
+              </div>
+            </div>      
+            
+          </li>
+          <li class="yuimenubaritem"><a class="yuimenubaritemlabel" href="#">Second Menu</a>
+          
+            <div class="yuimenu">
+              <div class="bd">                    
+                <ul>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fourth Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fifth Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Sixth Menu Item</a></li>
+                </ul>
+              </div>
+            </div>
+                                
+          </li>
+          <li class="yuimenubaritem"><a class="yuimenubaritemlabel" href="#">Third Menu</a>
+          
+            <div class="yuimenu">
+              <div class="bd">                    
+                <ul>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fourth Menu Item</a>
+
+                    <div class="yuimenu">
+                      <div class="bd">
+                        <ul class="first-of-type">
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Sub Item</a></li>
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Sub Item</a></li>
+                          <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Sub Item</a></li>
+                        </ul>            
+                      </div>
+                    </div>                    
+
+                  </li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Fifth Menu Item</a></li>
+                </ul>                    
+              </div>
+            </div>                                        
+
+          </li>
+          <li class="yuimenubaritem"><a class="yuimenubaritemlabel" href="#">Fourth Menu</a>
+
+            <div class="yuimenu">
+              <div class="bd">                                        
+                <ul>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">First Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Second Menu Item</a></li>
+                  <li class="yuimenuitem"><a class="yuimenuitemlabel" href="#">Third Menu Item</a></li>
+                </ul>
+              </div>
+            </div>                                        
+
+          </li>
+        </ul>     
+               
+      </div>
+    </div>
